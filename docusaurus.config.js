4bc6a3b239499f2038587446238d571f487d6a2b
Deep <deep@Deeps-MacBook-Pro.local>
2025-05-17 15:03:15 +0530

    V2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,15 +9,15 @@ const config = {
   favicon: 'img/favicon.svg',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sf-ranjan.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/my-docs/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'BigOwl', // Usually your GitHub org/user name.
-  projectName: 'Notification Doc', // Usually your repo name.
+  organizationName: 'sf-ranjan', // Usually your GitHub org/user name.
+  projectName: 'my-doc', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
