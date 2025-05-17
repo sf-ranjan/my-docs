6723e31fb694dd00e1e8fcf7ae5c4b709134558d
Deep <deep@Deeps-MacBook-Pro.local>
2025-05-17 19:29:34 +0530

    base url changed to support custom domain

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -12,7 +12,7 @@ const config = {
   url: 'https://sf-ranjan.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/my-docs/',
+  baseUrl: '/',
   deploymentBranch: 'gh-pages',
 
   // GitHub pages deployment config.
