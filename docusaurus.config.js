3fad323a205ff5d32e11902fb71caac4ce26ea8f
Deep <deep@Deeps-MacBook-Pro.local>
2025-05-17 16:21:33 +0530

    V2

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,8 +85,28 @@ const config = {
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
+        additionalLanguages: ["latex"],
       },
     }),
+    stylesheets: ["https://fonts.googleapis.com/icon?family=Material+Icons"],
+     themes: [
+    // ... Your other themes.
+    [
+      require.resolve("@easyops-cn/docusaurus-search-local"),
+      /** @type {import("@easyops-cn/docusaurus-search-local").PluginOptions} */
+      ({
+        // ... Your options.
+        // `hashed` is recommended as long-term-cache of index file is possible.
+        hashed: true,
+
+        // For Docs using Chinese, it is recomended to set:
+        // language: ["en", "zh"],
+
+        // If you're using `noIndex: true`, set `forceIgnoreNoIndex` to enable local index:
+        // forceIgnoreNoIndex: true,
+      }),
+    ],
+  ],
 };
 
 export default config;
