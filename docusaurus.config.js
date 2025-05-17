1776f9c8148ae1b7701337f58b0c223c4778d826
Deep <deep@Deeps-MacBook-Pro.local>
2025-05-17 18:31:55 +0530

    deleted the lock file

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -19,6 +19,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'sf-ranjan', // Usually your GitHub org/user name.
   projectName: 'my-doc', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
