1f2fa31742fb3058bf9348cc611ce1577b5467db
Deep Ranjan <dranjan01@outlook.com>
2025-05-18 16:59:54 +0530

    blogs enabled and author added

--- src/pages/index.js
+++ src/pages/index.js
@@ -19,7 +19,7 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/author/About Me/intro">
+            to="/docs/author/About Me/Intro">
             About Me
           </Link>
         </div>
