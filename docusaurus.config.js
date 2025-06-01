becffed75cee43613025124877dad8c45b4496b7
Deep Ranjan <dranjan01@outlook.com>
2025-06-01 15:43:36 +0530

    blog code change

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,7 +43,7 @@ const config = {
 			/** @type {import('@docusaurus/preset-classic').Options} */
 			({
 				gtag: {
-					trackingID: "G-KD8KDXVJ9K",
+					trackingID: "G-6SHECS696V",
 					anonymizeIP: true,
 				},
 				googleTagManager: {
